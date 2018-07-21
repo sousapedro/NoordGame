56120fabdeb81908116968bf6c7399498a432662
Pedro Ivo Sousa <pedroivo31@gmail.com>
2018-07-21 00:16:21 -0300

    + player moving

--- NoordGameJam/Assets/Scripts/Player.cs
+++ NoordGameJam/Assets/Scripts/Player.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public static float _BaseSpeed = 10f;//5f
+    public float Speed = _BaseSpeed;
+
+    private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void FixedUpdate()
+    {		
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+
+        rb.velocity  = movement * Speed;
+    }
 }
