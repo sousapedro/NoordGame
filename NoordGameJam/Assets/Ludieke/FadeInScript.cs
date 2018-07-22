ca997574e628f20f6d57e74c4f0fdecdeef1702f
Pedro Ivo Sousa <pedroivo31@gmail.com>
2018-07-22 17:09:06 -0300

    + ludieke screen + title screen

---
+++ NoordGameJam/Assets/Ludieke/FadeInScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeInScript : MonoBehaviour
+{
+    public Animator anim;
+
+    private int levelToStart;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToStart = levelIndex;
+        anim.SetTrigger("fadeStart");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToStart);
+    }
+}
---
+++ NoordGameJam/Assets/Menu/FadeMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeMenuScript : MonoBehaviour
+{
+    public Animator anim;
+
+    private int levelToStart;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(2);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToStart = levelIndex;
+        anim.SetTrigger("fadeStart");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToStart);
+    }
+}
